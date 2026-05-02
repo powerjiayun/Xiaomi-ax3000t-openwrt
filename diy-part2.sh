6f41aa54d9f743c62312020965fbc1eebaf2f167
ninesun99 <ninesun_ninesun@outlook.com>
2026-05-02 18:48:27 +0800

    Modify default gateway and theme in diy-part2.sh
    Update default gateway and theme in OpenWrt script.
    
    Signed-off-by: ninesun99 <ninesun_ninesun@outlook.com>

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+# 将默认网关从 192.168.1.1 修改为你习惯的 192.168.8.1。注意 ZC360 模板默认 IP 是 192.168.1.1
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 强制将默认主题设为 Argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 核心防坑：从你原来的脚本移植，融合 237 大佬的闭源驱动底包
+mv .config .config.bak
+cp -f defconfig/mt7981-ax3000.config .config
+cat .config.bak >> .config
